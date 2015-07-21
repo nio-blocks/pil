46e2e1833195fe787b09e75d37a834b8aa59e4bc
Chris Brackert <cbrackert@gmail.com>
2015-07-21 23:29:26 +0000

    Add unit tests for PILNewImage

--- tests/test_pil_new_image_block.py
+++ tests/test_pil_new_image_block.py
@@ -1,10 +1,11 @@
+from PIL import Image
 from collections import defaultdict
 from nio.common.signal.base import Signal
 from nio.util.support.block_test_case import NIOBlockTestCase
-from ..pil_block import Pil
+from ..pil_new_image_block import PILNewImage
 
 
-class TestPIL(NIOBlockTestCase):
+class TestPILNewImage(NIOBlockTestCase):
 
     def setUp(self):
         super().setUp()
@@ -14,14 +15,16 @@ class TestPIL(NIOBlockTestCase):
     def signals_notified(self, signals, output_id='default'):
         self.last_notified[output_id].extend(signals)
 
-    def test_pass(self):
-        pass
-
-    def test_process_signals(self):
-        blk = Pil()
+    def test_notified_signals(self):
+        blk = PILNewImage()
         self.configure_block(blk, {})
         blk.start()
         blk.process_signals([Signal()])
         blk.stop()
         self.assert_num_signals_notified(1)
-        self.assertDictEqual(self.last_notified['default'][0].to_dict(), {})
+        self.assertEqual(['image'],
+                         list(self.last_notified['default'][0].to_dict().keys()))
+        self.assertEqual(Image.Image,
+                         type(self.last_notified['default'][0].image))
+        self.assertEqual((64, 48),
+                         self.last_notified['default'][0].image.size)
