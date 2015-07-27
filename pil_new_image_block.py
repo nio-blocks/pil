bf2d02938fecda93dbac5f9e487acf5d509a00f4
Chris Brackert <cbrackert@gmail.com>
2015-07-27 11:25:34 -0700

    Add size property for NewImage block

--- pil_new_image_block.py
+++ pil_new_image_block.py
@@ -1,7 +1,13 @@
 from PIL import Image
 from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
-from nio.metadata.properties import VersionProperty
+from nio.metadata.properties import ExpressionProperty, VersionProperty, \
+        ObjectProperty, PropertyHolder
+
+
+class Size(PropertyHolder):
+    x = ExpressionProperty(default='64')
+    y = ExpressionProperty(default='48')
 
 
 @Discoverable(DiscoverableType.block)
@@ -10,9 +16,25 @@ class PILNewImage(Block):
     """ Use PIL to create a new Image and store the Image in a signal """
 
     version = VersionProperty('0.1.0')
+    size = ObjectProperty(Size, default=Size())
 
     def process_signals(self, signals, input_id='default'):
         for signal in signals:
-            image = Image.new('1', (64, 48))
-            signal.image = image
+            try:
+                self._create_image(signal)
+            except:
+                self._logger.exception('Failed to create image')
         self.notify_signals(signals, output_id='default')
+
+    def _create_image(self, signal):
+        x, y = self._image_size(signal)
+        image = Image.new('1', (x, y))
+        signal.image = image
+
+    def _image_size(self, signal):
+        try:
+            x = int(self.size.x(signal))
+            y = int(self.size.y(signal))
+        except:
+            raise Exception('Failed to evaluate block properties')
+        return x, y
--- tests/test_pil_new_image_block.py
+++ tests/test_pil_new_image_block.py
@@ -15,7 +15,7 @@ class TestPILNewImage(NIOBlockTestCase):
     def signals_notified(self, signals, output_id='default'):
         self.last_notified[output_id].extend(signals)
 
-    def test_notified_signals(self):
+    def test_defaults(self):
         blk = PILNewImage()
         self.configure_block(blk, {})
         blk.start()
@@ -28,3 +28,27 @@ class TestPILNewImage(NIOBlockTestCase):
                          type(self.last_notified['default'][0].image))
         self.assertEqual((64, 48),
                          self.last_notified['default'][0].image.size)
+
+    def test_size_prop(self):
+        blk = PILNewImage()
+        self.configure_block(blk, {'size': {'x': 1}})
+        blk.start()
+        blk.process_signals([Signal()])
+        blk.stop()
+        self.assert_num_signals_notified(1)
+        self.assertEqual(['image'],
+                         list(self.last_notified['default'][0].to_dict().keys()))
+        self.assertEqual(Image.Image,
+                         type(self.last_notified['default'][0].image))
+        self.assertEqual((1, 48),
+                         self.last_notified['default'][0].image.size)
+
+    def test_invalid_size_prop(self):
+        blk = PILNewImage()
+        self.configure_block(blk, {'size': {'x': None}})
+        blk.start()
+        blk.process_signals([Signal()])
+        blk.stop()
+        self.assert_num_signals_notified(1)
+        self.assertTrue(
+            'image' not in self.last_notified['default'][0].to_dict())
