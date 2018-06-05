9a6f6ac0ea724a96ba4e72d6f33449736d3a7d07
neutralioJenkins <jenkins@n.io>
2018-06-05 09:33:43 -0600

    Bump version to v0.1.0

--- pil_image_from_array_block.py
+++ pil_image_from_array_block.py
@@ -8,7 +8,7 @@ class PILImageFromArray(Block):
 
     """ Convert an array to a PIL Image """
 
-    version = VersionProperty('0.1.0')
+    version = VersionProperty("0.1.0")
     input_array = Property(title='Array to Convert', default='{{ $image }}')
 
     def process_signals(self, signals, input_id='default'):
