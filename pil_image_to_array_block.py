e4c1175bf95c860e324842b33b19c58b260cb2c7
neutralioJenkins <jenkins@n.io>
2018-06-05 09:34:50 -0600

    Bump version to v0.1.0

--- pil_image_to_array_block.py
+++ pil_image_to_array_block.py
@@ -8,7 +8,7 @@ class PILImageToArray(Block):
 
     """ Convert a PIL Image to a numpy array """
 
-    version = VersionProperty('0.1.0')
+    version = VersionProperty("0.1.0")
 
     def process_signals(self, signals, input_id='default'):
         for signal in signals:
